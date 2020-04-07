d15dd1690ea0d22e9da5769be079293a8f476ef1
Rodrigo Nahum <34604938+rnahumaf@users.noreply.github.com>
2020-04-07 11:59:13 -0300

    UPDATE TO INCLUDE PLOTLY

--- Predictions/Death_predict.R
+++ Predictions/Death_predict.R
@@ -1,7 +1,10 @@
+####   L I B R A R I E S   &   F U N C T I O N S   ####
+
 # Libraries
 library(minpack.lm)
 library(ggplot2)
 library(data.table)
+library(plotly)
 
 # Functions
 prepare <- function(mortes, column = 1, prim = 1, date = F){
@@ -15,15 +18,11 @@ prepare <- function(mortes, column = 1, prim = 1, date = F){
     df.mortes <<- data.frame(day = day.mortes, mortes = red.mortes)
   }
 }
-
 Cgeneral <- function(c0, r, m, proj){
   return((r*(0:(proj-1))/m + c0^(1/m))^m)
 }
 
-# prepare(df, death col, start, date col)
-prepare(mortes, 3, 1, 1) 
-
-# Dataset
+# Data
 mortes <- fread("Data	Casos	Óbitos
 2020-02-26	1	0
 2020-02-27	1	0
@@ -64,38 +63,43 @@ mortes <- fread("Data	Casos	Óbitos
 2020-04-02	7910	299")
 mortes$Data <- as.Date(mortes$Data, "%Y-%m-%d")
 
+####   C A L C U L A T I O N S   ####
+
+# prepare(df, death col, start, date col)
+prepare(mortes, 3, 1, 1) 
 # day zero
 c0 <- df.mortes[1, 3]
-
 # create model
 mod.mortes <- nlsLM(mortes ~ (r*day/m + c0^(1/m))^m, 
                     data = df.mortes,
                     start = list(r = 0.5, m = 0.5))
-
 # assign confidence intervals
 lower.r <- confint(mod.mortes)[1,1]
 higher.r <- confint(mod.mortes)[1,2]
 lower.m <- confint(mod.mortes)[2,1]
 higher.m <- confint(mod.mortes)[2,2]
+real <- df.mortes[, 3]
+# Length of prediction to be used
+len.pred <- 4
 
-####### Plot using base R (problem: can't plot dates)
+####   P L O T S   ####
+####   B A S E   R   ####
+
 # plot confidence intervals + original data
-x <- c(1:nrow(df.mortes))
-y1 <- Cgeneral(1, lower.r, lower.m, nrow(df.mortes))
-y2 <- Cgeneral(1, higher.r, higher.m, nrow(df.mortes))
-plot(x, y1, type="l", col=rgb(0,0,0, alpha=0.5), xlim = c(0, 20), ylim = c(0, 1000))
+x <- c(1:(nrow(df.mortes)+len.pred))
+y1 <- Cgeneral(1, lower.r, lower.m, nrow(df.mortes)+len.pred)
+y2 <- Cgeneral(1, higher.r, higher.m, nrow(df.mortes)+len.pred)
+plot(x, y1, log="y", type="l", col=rgb(0,0,0, alpha=0.5), xlim = c(1, 25), ylim = c(1, 1000))
 lines(x, y2, type="l", col=rgb(0,0,0, alpha=0.5))
 polygon(c(x, rev(x)), c(y1,rev(y2)),col="skyblue")
+lines(real, type="p")
 
-# plot real data
-lines(df.mortes[, 3], type="l", lwd=5, col=rgb(1,0,0, alpha=0.5))
+####   P L O T S   ####
+####   G G P L O T   &   P L O T L Y   ####
 
-####### Plot using ggplot2 (problem: more complicated)
 # plot with dates
-library(ggplot2)
 lower <- Cgeneral(1, lower.r, lower.m, nrow(df.mortes)+4)
 higher <- Cgeneral(1, higher.r, higher.m, nrow(df.mortes)+4)
-real <- df.mortes[, 3]
 forecast <- Cgeneral(1, coef(mod.mortes)[1], coef(mod.mortes)[2], nrow(df.mortes)+4)
 max.len = max(length(higher), length(lower), length(real))
 mydata = data.frame(date = seq(as.Date("2020-03-17"), as.Date("2020-03-17")+max.len-1, by="days"),
@@ -103,12 +107,24 @@ mydata = data.frame(date = seq(as.Date("2020-03-17"), as.Date("2020-03-17")+max.
                     higher = c(higher, rep(NA, max.len - length(higher))),
                     real = c(real, rep(NA, max.len-length(real))),
                     forecast = c(forecast, rep(NA, max.len-length(forecast))))
-ggplot(mydata, aes(x=date, y = real)) +
-  geom_line(aes(y = lower)) + 
-  geom_line(aes(y = higher)) + 
-  geom_ribbon(aes(ymin = lower, ymax = higher), fill = "gray", alpha = .5) +
-  geom_line(aes(y = forecast), color="darkgray", size = 2) +
-  geom_point(aes(y = real),color="black", size = 3) +
-  ylim(0, 1250) + xlim(as.Date("2020-03-17"), as.Date("2020-03-17")+max.len-1) +
-  theme_bw()+
-  ylab("Previsão de mortes") + xlab("Data")
+p <-  ggplot(mydata, aes(x=date, y = real)) +
+      geom_line(aes(y = lower)) + 
+      geom_line(aes(y = higher)) + 
+      geom_ribbon(aes(ymin = lower, ymax = higher), fill = "gray", alpha = .5) +
+      geom_line(aes(y = forecast), color="darkgray", size = 2) +
+      geom_point(aes(y = real),color="black", size = 2) +
+      ylim(0, 1250) + xlim(as.Date("2020-03-17"), as.Date("2020-03-17")+max.len-1) +
+      theme_bw() +
+      ylab("Previsão de mortes") + xlab("Data") +
+      scale_y_log10()
+# Check result
+ggplotly(p)
+
+####   E X P O R T   T O   P L O T L Y   ####
+
+# Export to Plotly.com for web publication
+# https://chart-studio.plotly.com/settings/api#/
+# https://plotly.com/r/sending-data-to-charts/
+Sys.setenv("plotly_username" = "xxx")
+Sys.setenv("plotly_api_key" = "xxx")
+api_create(ggplotly(p), filename='test')
