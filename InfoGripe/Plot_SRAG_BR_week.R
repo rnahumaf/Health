b6fd689374bac505ce9370e7da25420b36170a88
Rodrigo Nahum <34604938+rnahumaf@users.noreply.github.com>
2020-04-17 19:09:53 -0300

    Ajuste estético

--- InfoGripe/Plot_SRAG_BR_week.R
+++ InfoGripe/Plot_SRAG_BR_week.R
@@ -13,9 +13,9 @@ ggplot(InfoGripe[which(InfoGripe$UF == "Brasil"),],
        aes(x = semana, y = casos,  color = ano)) +
   geom_point(aes(size = casos, alpha = casos)) +
   scale_colour_manual(values=c("red", rep('black', 10), "blue")) +
-  annotate("Text", x = "37", y = 9000, label = "H1N1 pandêmico \n 2009", size = 5) +
-  annotate("Text", x = "7", y = 8000, label = "SARS-CoV-2 \n 2020", size = 5) +
-  ggtitle("Casos respiratórios agudos graves no Brasil \n entre 2009 e 2020 (fonte: InfoGripe)") +
+  annotate("Text", x = "32", y = 9000, label = "Gripe A\n 2009", size = 5) +
+  annotate("Text", x = "13", y = 10000, label = "COVID-19\n 2020", size = 5) +
+  ggtitle("Quadros respiratórios agudos graves no Brasil \n entre 2009 e 2020 (fonte: InfoGripe)") +
   ylab("Número de casos") +
   xlab("Semana do ano") +
   labs(size="Casos", 
@@ -23,13 +23,13 @@ ggplot(InfoGripe[which(InfoGripe$UF == "Brasil"),],
        alpha="Casos") +
   scale_x_discrete(labels = seq(1, 52, 3), breaks = seq(1, 52, 3)) +
   scale_y_continuous(breaks = seq(0, 12000, 1000)) +
-  guides(color = guide_legend(override.aes = list(size=5))) +
+  guides(color = guide_legend(override.aes = list(size=3))) +
   theme_bw() +
   theme(axis.text.y = element_text(size=10),
         axis.text.x = element_text(size=10),
         axis.title = element_text(size=15),
         plot.title = element_text(size=20, hjust = 0.5),
-        legend.title = element_text(size=15),
-        legend.text=element_text(size=15))
+        legend.title = element_text(size=13),
+        legend.text=element_text(size=10))
 
 ggsave("InfoGripe.png", width = 20, height = 20, units = "cm", dpi = 300)
