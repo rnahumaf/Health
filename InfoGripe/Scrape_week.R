760399065abdccdd5cb5b43bb74e2f5de1772220
Rodrigo Nahum <34604938+rnahumaf@users.noreply.github.com>
2020-04-17 16:28:42 -0300

    Correction to obtain case number
    Previously, the script retrieved only the incidence data. Now it retrieves the number of cases.

--- InfoGripe/Scrape_week.R
+++ InfoGripe/Scrape_week.R
@@ -9,10 +9,11 @@ TBL <- data.frame(data.territory_name = NA,
                   data.value = NA,
                   ano = NA)
 
-# Add data to TBL
+# Add data to TBL (CASE NUMBER)
 for(ano in 2009:2020){
   for(semana in 1:52){
-    URL <- fromJSON(paste0('http://info.gripe.fiocruz.br/data/detailed/1/1/', ano, '/', semana, '/1/Brasil/data-table'))
+    # Replace below with 'http://info.gripe.fiocruz.br/data/detailed/1/2/' to obtain incidence data
+    URL <- fromJSON(paste0('http://info.gripe.fiocruz.br/data/detailed/1/2/', ano, '/', semana, '/1/Brasil/data-table'))
     URL <- data.frame(URL)
     URL$ano <- ano
     TBL <- rbind(TBL, URL)
@@ -20,5 +21,7 @@ for(ano in 2009:2020){
   }
 }
 
+TBL <- TBL[2:nrow(TBL),]
+
 # Export to Excel file
-write_xlsx(TBL, "InfoGripe_2020_14.xlsx")
+write_xlsx(TBL, "InfoGripe_2020_14_casos.xlsx")
